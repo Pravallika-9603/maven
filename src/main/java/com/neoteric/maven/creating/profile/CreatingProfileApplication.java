af24c7ad03304e535ed24158e51103b4704b4ec8
Pravallika <pravali9603@gmail.com>
2024-07-03 11:21:25 +0530

    first commit

---
+++ src/main/java/com/neoteric/maven/creating/profile/CreatingProfileApplication.java
@@ -0,0 +1,13 @@
+package com.neoteric.maven.creating.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatingProfileApplication.class, args);
+	}
+
+}
